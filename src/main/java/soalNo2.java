e9401f71829430703318e4cc9b9a8fa334b756c7
Bangrawon <agungwar00@gmail.com>
2020-04-19 12:35:21 +0700

    fixing up number two

--- src/main/java/soalNo2.java
+++ src/main/java/soalNo2.java
@@ -11,21 +11,41 @@ import java.util.Stack;
  *
  * @author rahardian
  */
-public class soalNo2 {
+public class soalNo2<T> extends Stack<T> {
+    private int maxSize;
+
+    public soalNo2(int size) {
+        super();
+        this.maxSize = size;
+    }
+
+    @Override
+    public T push(T object) {
+        //If the stack is too big, remove elements until it's the right size.
+        if (this.size() >= maxSize) {
+            System.out.println("STACK IS FULL");
+            return null;
+        } else {
+            return super.push(object);
+        }
+    }
+    
     public static void main(String[] args) {
-        System.out.println("STACK IS FULL");
-        Stack<String> stackOfCards = new Stack<>();
-        System.out.println("---------PUSH--------");
+        Stack<String> stackOfCards = new soalNo2<>(3);
+        
         stackOfCards.push("5");
         stackOfCards.push("3");
         stackOfCards.push("2");
-        for (String card : stackOfCards) {
+        stackOfCards.push("1");
+
+        System.out.println("---------PUSH--------");
+        stackOfCards.forEach((card) -> {
             System.out.println(card);
-        }
+        });
         System.out.println("---------POP--------");
         stackOfCards.pop();
-        for (String card : stackOfCards) {
+        stackOfCards.forEach((card) -> {
             System.out.println(card);
-        }
+        });
     }
 }
